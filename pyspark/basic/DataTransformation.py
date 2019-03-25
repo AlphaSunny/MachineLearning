e8477cee7a50fe381fba90c9a7e2f7447682047b
yangguang <epsilonsunny@gmail.com>
2019-03-25 13:26:22 +0800

    on

--- pyspark/basic/DataTransformation.py
+++ pyspark/basic/DataTransformation.py
@@ -6,20 +6,15 @@ from pyspark.ml.feature import VectorAssembler
 spark = SparkSession.builder.appName('data').getOrCreate()
 
 df = spark.read.csv('hdfs:///user/maria_dev/MachineLearning/fake_customers.csv',inferSchema=True,header=True)
-
-df.show()
-
-
-
 df = spark.createDataFrame(
     [(0, "a"), (1, "b"), (2, "c"), (3, "a"), (4, "a"), (5, "c")],
     ["user_id", "category"])
 
+df.show()
+
 indexer = StringIndexer(inputCol="category", outputCol="categoryIndex")
 indexed = indexer.fit(df).transform(df)
 indexed.show()
-
-
 
 dataset = spark.createDataFrame(
     [(0, 18, 1.0, Vectors.dense([0.0, 10.0, 0.5]), 1.0)],
